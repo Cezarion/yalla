f734328be526d879459d5b0e3f655fad8bcced9f
Thomas Frenot <thomas.frenot@fabernovel.com>
2016-11-23 17:29:30 +0100

    add usefull and suggested bundles list

--- bin/symfony/init.sh
+++ bin/symfony/init.sh
@@ -21,11 +21,31 @@ ln -s web app/htdocs
 echo "${GREEN} [ok] ${NORMAL}Create symlink local.yml from symfony parameters.yml"
 ln -s ../app/app/config/parameters.yml shared/local.yml
 
-#remove all other initialisation project
+# Remove all other initialisation project
 remove_other_project_init $PROJECT_TYPE
 echo "${GREEN} [ok] ${NORMAL}Remove others project initialisation files"
 
-#first commit with app content, only if current repo not skeleton
+# Add some usefull bundles
+BUNDLES_LIST=`cat bundles_list_usefull`
+for entry in $BUNDLES_LIST
+do
+    app/composer require $entry
+done
+
+# Ask developer to add some suggested bundles
+BUNDLES_LIST=`cat bundles_list_suggestion`
+for entry in $BUNDLES_LIST
+do
+	read -p "Do you need bundle ${entry}: ? [Y/n]" -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        echo "${GREEN} [ok] ${NORMAL}Add ${entry} to your project"
+        app/composer require $entry
+    fi
+done
+
+# First commit with app content, only if current repo not skeleton
 if [ `git config --get remote.origin.url | cut -d / -f 2` != "project-skeleton.git" ]; then
     git add .
     git commit -m "Initialisation ${PROJECT_TYPE} project"
