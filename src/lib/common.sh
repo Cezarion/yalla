7bfe9548f9898224572a697a8deb0308fdaa5d44
Mathias Gorenflot <mathias.gorenflot@fabernovel.com>
2017-08-31 00:47:19 +0200

    Fix massive loading

--- src/lib/common.sh
+++ src/lib/common.sh
@@ -6,8 +6,8 @@ trap 'echo "Aborting due to errexit on line $LINENO. Exit code: $?" >&2' ERR
 
 
 # Load common vars and functions
-for file in "${APP_YALLA_LIB}/*"
-do
-    . $file
-done
-
+. "${APP_YALLA_LIB}/variables.sh"
+. "${APP_YALLA_LIB}/colors.sh"
+. "${APP_YALLA_LIB}/helpers.sh"
+. "${APP_YALLA_LIB}/functions.sh"
+. "${APP_YALLA_LIB}/yalla.functions.sh"
