7ee7fa5d81d7df11bd2298bee039196fc9fa4386
Mathias Gorenflot <mathias.gorenflot@fabernovel.com>
2017-08-30 14:48:05 +0200

    Fix typo

--- src/cli/cli-install.sh
+++ src/cli/cli-install.sh
@@ -20,7 +20,7 @@ curl -o /usr/local/bin/yalla  https://buzzaka:Buzz06\$dev@yalla-dl.fabernovel.co
 chmod +x /usr/local/bin/yalla;
 
 # Download autocomplete
-curl -o "${HOME}/.yalla.autocomplete"  https://buzzaka:Buzz06\$dev@yalla-dl.fabernovel.co/yall.autocomplete.;
+curl -o "${HOME}/.yalla.autocomplete"  https://buzzaka:Buzz06\$dev@yalla-dl.fabernovel.co/yalla.autocomplete;
 
 #chmod +x "${HOME}/.yalla.autocomplete";
 
