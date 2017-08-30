51481fa2d86bc5531e428f931ec32c4cf5209953
Mathias Gorenflot <mathias.gorenflot@fabernovel.com>
2017-08-31 00:12:30 +0200

    Refactor folders organisation

--- src/lib/common.sh
+++ src/lib/common.sh
@@ -4,10 +4,10 @@
 set -e
 trap 'echo "Aborting due to errexit on line $LINENO. Exit code: $?" >&2' ERR
 
-SRC=$(cd $(dirname "$0"); pwd)
 
-. "${SRC}/src/lib/variables.sh"
-. "${SRC}/src/lib/colors.sh"
-. "${SRC}/src/lib/helpers.sh"
-. "${SRC}/src/lib/functions.sh"
-. "${SRC}/src/lib/yalla.functions.sh"
+# Load common vars and functions
+for file in "${APP_YALLA_LIB}/*"
+do
+    . $file
+done
+
