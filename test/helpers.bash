97a08e8bd386e468cd254807936147101838ea5d
Cezarion <cezarion@cezarion.net>
2017-10-17 15:10:49 +0200

    Start to write (and debug) tests

---
+++ test/helpers.bash
@@ -0,0 +1,37 @@
+assert_exists() {
+  assert [ -e "$1" ]
+}
+
+refute_exists() {
+  assert [ ! -e "$1" ]
+}
+
+assert_contains() {
+  local item
+  for item in "${@:2}"; do
+    if [[ "$item" == "$1" ]]; then
+      return 0
+    fi
+  done
+
+  batslib_print_kv_single_or_multi 8 \
+        'expected' "$1" \
+        'actual'   "$(echo ${@:2})" \
+      | batslib_decorate 'item was not found in the array' \
+      | fail
+}
+
+setupYallaEnv() {
+  export YALLA_DIRECTORY="$(mktemp -d)"
+  export YALLA_HOME="$(mktemp -d)"
+  export HOME=$YALLA_HOME
+}
+
+teardownYallaEnv() {
+  if [ $BATS_TEST_COMPLETED ]; then
+    rm -rf $YALLA_DIRECTORY
+    rm -rf $YALLA_HOME
+  else
+    echo "** Did not delete $YALLA_DIRECTORY, as test failed **"
+  fi
+}
---
+++ test/test-help.bats
@@ -0,0 +1,36 @@
+#!./test/libs/bats/bin/bats
+
+load 'libs/bats-support/load'
+load 'libs/bats-assert/load'
+load 'helpers'
+
+setup() {
+  setupYallaEnv
+}
+
+teardown() {
+  teardownYallaEnv
+}
+
+yalla="./src/cli/yalla"
+
+@test "Should print help successfully if requested" {
+  run $yalla --help
+
+  assert_success
+  assert_line "Main commands :"
+}
+
+@test "Should print help if no arguments are provided, and exit unsuccessfully" {
+  run $yalla
+
+  assert_failure
+  assert_line "Main commands :"
+}
+
+@test "Should print help if an unrecognized command is used, and exit unsuccessfully" {
+  run $yalla imaginary-command
+
+  assert_failure
+  assert_line "Main commands :"
+}
