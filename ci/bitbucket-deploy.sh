5cd80eecc6171b918bca0f5c9227d735556dab5a
Cezarion <cezarion@cezarion.net>
2017-11-06 12:04:11 +0100

    test version

--- ci/bitbucket-deploy.sh
+++ ci/bitbucket-deploy.sh
@@ -7,9 +7,9 @@ echo "Make git archive for commit ${BITBUCKET_COMMIT}"
 version1=`git diff HEAD^..HEAD -- "$(git rev-parse --show-toplevel)"/src/cli/yalla | grep '^\+.*YALLA_VERSION' | sed -E 's/[^0-9\.]//g'` | echo $version1
 version2=`git diff HEAD^..HEAD -- "$(git rev-parse --show-toplevel)"/src/cli/yalla | grep '^\+.*YALLA_VERSION' | sed -s 's/[^0-9\.]//g'` | echo $version2
 
-if [ "$version1" != "" ]; then
-    git tag -a "v$version1" -m "`git log -1 --format=%s`"
-    echo "Created a new tag, v$version1"
+if [ "$version" != "" ]; then
+    git tag -a "v$version" -m "`git log -1 --format=%s`"
+    echo "Created a new tag, v$version"
     git push && git push --tags
 else
     echo "No new version"
