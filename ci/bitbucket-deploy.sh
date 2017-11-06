d406e4e1d05dd8b465bf9d1e6d854ce5ed7e23ea
Cezarion <cezarion@cezarion.net>
2017-11-06 11:20:51 +0100

    test version detection

--- ci/bitbucket-deploy.sh
+++ ci/bitbucket-deploy.sh
@@ -6,5 +6,13 @@ echo "Make git archive for commit ${BITBUCKET_COMMIT}"
 git archive --format=tar.gz -o deploy.tgz $BITBUCKET_COMMIT
 version=`git diff HEAD^..HEAD -- "$(git rev-parse --show-toplevel)"/src/cli/yalla | grep '^\+.*YALLA_VERSION' | sed -E 's/[^0-9\.]//g'` | echo $version
 version=`git diff HEAD^..HEAD -- "$(git rev-parse --show-toplevel)"/src/cli/yalla | grep '^\+.*YALLA_VERSION' | sed -s 's/[^0-9\.]//g'` | echo $version
+
 echo "Transfert archive"
+
+if [ "$version" != "" ]; then
+    echo "Created a new tag, v$version"
+else
+    echo "No new version"
+fi
+
 scp deploy.tgz deploy@37.187.57.175:/home/webdev/yalla/site/dl/
