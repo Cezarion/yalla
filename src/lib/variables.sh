f0488dbc9636d0437dddbbd2210f004c1efde963
Cezarion <cezarion@cezarion.net>
2017-12-09 12:00:34 +0100

    Version bump to 0.1.7

--- src/lib/variables.sh
+++ src/lib/variables.sh
@@ -5,7 +5,7 @@ trap 'echo "Aborting due to errexit on line $LINENO. File $(cd $(dirname "$0"); 
 
 ## Never edit manually this line.
 ## Run sh ci/bumversion.sh instead
-readonly YALLA_VERSION=0.1.6
+readonly YALLA_VERSION=0.1.7
 
 # Folder path where application code is located
 readonly APPLICATION_PATH_NAME='application'
